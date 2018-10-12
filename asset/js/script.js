b606e275c64ac822f973edbb86bb4f7d0503d6da
JorellanaF <00103717@uca.edu.sv>
2018-10-12 16:41:23 -0600

    segunda parte

--- asset/js/script.js
+++ asset/js/script.js
@@ -6,5 +6,9 @@ container.style.width="200px";
 container.style.height="200px";
 container.innerHTML="<input type=text placeholder='Escriba su texto'>";
 
-var containers = document.getElementsByClassName(".containe c2");
-containers.;
+var containers=document.querySelectorAll(".c2");
+
+for(i=0;i<containers.length;i++){
+    containers[i].style.backgroundColor= '#009999';
+    containers[i].innerHTML="<input type='text' placeholder='EScriba sdu texto'>";
+};
