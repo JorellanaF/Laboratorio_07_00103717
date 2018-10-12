81ccdf9ffe4b8afa7c240387fc16308c10b82c49
JorellanaF <00103717@uca.edu.sv>
2018-10-12 02:08:13 -0600

    archivos

---
+++ asset/js/script.js
@@ -0,0 +1,10 @@
+var container = document.getElementById("c1");
+container.innerText="¡Hola Mundo!";
+container.style.backgroundColor="#000000";
+container.style.color="#1cb723";
+container.style.width="200px";
+container.style.height="200px";
+container.innerHTML="<input type=text placeholder='Escriba su texto'>";
+
+var containers = document.getElementsByClassName(".containe c2");
+containers.;
